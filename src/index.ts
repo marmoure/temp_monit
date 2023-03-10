1d041783dee1573d29ede776876149bd8598d754
marmoure <tinky93winky@gmail.com>
2023-03-11 00:34:10 +0100

    working version of sensor data reception

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import app  from './server';
 
 
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(4000, () => {
+  console.log('Server started on port 4000');
 });
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,49 @@
 import path from 'path';
 import express from 'express';
+import prisma from './database';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/update-sensor', async (req,res) => {
+app.get('/update-sensor', async (req, res) => {
   console.log(req.query);
-  const temperature = req.query.temperature;
+  const { temperature, device_id } = req.query;
+  if (!temperature || !device_id) return res.send("ERROR");
+  let device = await prisma.device.findUnique({
+    where: {
+      device_id: device_id as string
+    }
+  });
+  if (!device) {
+    device = await prisma.device.create({
+      data: {
+        device_id: device_id as string,
+        name: "TODO"
+      }
+    })
+  }
+
+  let newTemperature = await prisma.temperature.create({
+    data: {
+      value: temperature as string,
+      device: {
+        connect: {
+          id: device.id
+        }
+      }
+    }
+  })
+  if(! device || !newTemperature) return res.send("ERROR");
   res.send("OK");
 })
 
 
-app.get("/",(req, res) => {
-  res.sendFile(path.join(__dirname,"public","index.html"))
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"))
 });
 
 
